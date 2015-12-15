8baac2d9a9aaaf6ec92811255be9430a7f265f1f
Mikael Eriksson <me222wm@student.lnu.se>
2015-12-15 18:48:54 +0100

    Improved structure. Trying to save to locale storage.

--- src/js/traffic-reporter.js
+++ src/js/traffic-reporter.js
@@ -1,121 +1,69 @@
 "use strict";
-
 function init(){
-
-    $.ajaxSetup({
-        async: false
-    });
-
     TrafficReporter = new TrafficReporter();
 }
 
 var TrafficReporter = function(){
 
-
-
-    var category;
-    var that = this;
-    this.getAPIResponse();
     this.response;
-    this.markers = L.layerGroup();
-    this.map = this.renderMap();
+    this.markers;
+    this.map;
+    this.category;
+    this.markers = new L.layerGroup;
 
+    var that = this;
 
+    this.renderMap();
+
+     // If user selects category: set category, clear layers and list and get messages again.
     $(".btn").click(function(){
-
-        // Makes button change color if active.
-        if ($(this).hasClass("active")) {
-            category = undefined;
-            $(this).removeClass("active");
-        } else {
-            category = that.getCategory($(this).attr("id"));
-            $(".btn").removeClass("active");
-            $(this).addClass("active");
-        }
-
-        // Clears layer containing markers, then creates new layer.
-
-        // Render content with category.
-        that.renderContent(category);
-        that.setUpdateInterval(category);
+        that.setCategory(this);
+        $("#messages").empty();
+        $("#copyright").empty();
+        that.markers.clearLayers();
+        that.getResponse();
     });
 
-    this.renderContent();
-    this.setUpdateInterval();
+    this.getResponse();
 
-};
+    var intervalDelay = 100000;
 
-TrafficReporter.prototype.setUpdateInterval = function(category){
+    if (localStorage.getItem("response") !== null) {
 
-    var that = this;
+        var timestamp = JSON.parse(localStorage.getItem("response")).timestamp;
+        console.log(timestamp);
+        var now = Date.now();
+        console.log(now);
 
+        var elapsed = now - timestamp;
+
+        console.log(elapsed);
+
+        if (elapsed >= intervalDelay) {
+            console.log("updating with timestamp");
+            reload();
+        }
+    }
+
+
+    // Makes new request and redraws messages every 10 minutes.
     setInterval(function () {
+        reload();
+    }, intervalDelay);
+
+    function reload() {
+        console.log("updating");
         localStorage.clear();
-        that.getAPIResponse();
-        that.renderContent(category);
-    }, 500000);
-
-};
-
-TrafficReporter.prototype.renderContent = function(category){
-
-    this.map.removeLayer(this.markers);
-    this.markers = L.layerGroup();
-    var messages = this.getMessages(category);
-    this.renderTrafficMessages(messages);
-};
-
-
-TrafficReporter.prototype.getMessages = function(category) {
-
-    var messages = [];
-
-    if (category !== undefined) {
-        // Filtrates messages based on chosen category.
-        this.response.messages.forEach(function(message) {
-            if (message.category === category) {
-                messages.push(message);
-            }
-        });
-    } else {
-        // User has not chosen category. Return all messages.
-        messages = this.response.messages;
+        $("#messages").empty();
+        $("#copyright").empty();
+        that.markers.clearLayers();
+        that.getResponse();
     }
-
-    return messages;
 };
 
-// Returns category based on a-tag id.
-TrafficReporter.prototype.getCategory = function(id) {
-
-    var category;
-
-    switch (id) {
-        case "cat-road":
-            category = 0;
-            break;
-        case "cat-public":
-            category = 1;
-            break;
-        case "cat-planed":
-            category = 2;
-            break;
-        case "cat-other":
-            category = 3;
-            break;
-        default:
-            category = null;
-            break;
-
-    }
-
-    return category;
-};
-
-// Rendering map with leaflet and Open street map.
 TrafficReporter.prototype.renderMap = function() {
 
-    var map = L.map( 'map', {
+    this.map = L.map( 'map', {
         center: [62.0, 14.8],
         minZoom: 2,
         zoom: 5
@@ -123,107 +71,217 @@ TrafficReporter.prototype.renderMap = function() {
 
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+    }).addTo(this.map);
 
-    //map.fitBounds(this.markers);
-
-    return map;
 };
 
-// Calling and getting response from API.
-TrafficReporter.prototype.getAPIResponse = function() {
-
-    // http://stackoverflow.com/questions/13853016/jquery-getjson-how-to-avoid-requesting-the-json-file-on-every-refresh-caching
-
-    var url = "http://api.sr.se/api/v2/traffic/messages?format=json&pagination=false";
-
-    var response = localStorage.getItem("response");
+TrafficReporter.prototype.setCategory = function(button) {
 
     var that = this;
 
-    if (!response) {
+    // Makes button change color if active.
+    if ($(button).hasClass("active")) {
+        // If user clicks category that is already active, render all categories.
+        that.category = undefined;
+        $(button).removeClass("active");
+    } else {
+        // Get id of clicked a-tag.
+        var id = $(button).attr("id");
 
-        $.getJSON(url, function(data) {
-            response = data;
-            localStorage.setItem("response", JSON.stringify(response));
+        switch (id) {
+            case "cat-road":
+                that.category = 0;
+                break;
+            case "cat-public":
+                that.category = 1;
+                break;
+            case "cat-planed":
+                that.category = 2;
+                break;
+            case "cat-other":
+                that.category = 3;
+                break;
+            default:
+                that.category = undefined;
+                break;
+
+        }
+
+        $(".btn").removeClass("active");
+        $(button).addClass("active");
+    }
+};
+
+
+TrafficReporter.prototype.getResponse = function() {
+
+    var srAPI = "http://api.sr.se/api/v2/traffic/messages?format=json&pagination=false";
+    var that = this;
+    var copyright;
+    var storageKey = "response";
+
+    var storedResponse = localStorage.getItem(storageKey);
+
+    if (true) {
+        $.ajaxSetup({ cache: true });
+        $.getJSON(srAPI, function(data){
+
+            // Create timestamp to see when object was saved to locale storage.
+            data.timestamp = new Date().getTime().toString();
+
+            // Save response in local storage.
+            localStorage.setItem(storageKey, JSON.stringify(data));
+            renderMessages(data);
 
         }).fail(function(){
             $("<div class='api-error'>Trafikmeddelanden kunde inte hämtas</div>").insertBefore("#map-container");
             return false;
         });
-
     } else {
-        response = JSON.parse(response);
+        renderMessages(JSON.parse(storedResponse));
     }
 
-    this.response = response;
-};
+    function renderMessages(response) {
 
-// Method in charge of creating marker and list item for each traffic message.
-TrafficReporter.prototype.renderTrafficMessages = function(messages) {
+        var messages = response.messages;
+        var copyright = response.copyright;
 
-    var that = this;
-    var copyright = this.response;
+        $("<small>"+copyright+"</small>").appendTo("#copyright");
 
-    // Changing format of date with moment.js.
-    messages.forEach(function(message){
-        message.createddate = moment(message.createddate).format("YYYY-MM-DD HH:mm");
-    });
 
-    // Sorting by date and time.
-    messages.sort(function (a, b) {
-        if (a.createddate < b.createddate) {
-            return 1;
+        messages = sortMessages(messages);
+
+        // If category-button is clicked the messages is to be filtrated.
+        if (that.category !== undefined) {
+            messages = filtrateMessages(messages);
         }
-        if (a.createddate > b.createddate) {
-            return -1;
+
+        messages.forEach(function(message) {
+
+            var category = getCategoryAsString(message);
+            var priority = getPriorityAsString(message);
+
+            renderListItem(message, category, priority);
+            createMarker(message, category, priority);
+
+        });
+
+        that.markers.addTo(that.map);
+
+        $(".message").click(function(){
+            messageMarkerPopup($(this).attr("id"));
+        });
+
+        // Iterates through markers to find the one with same title as clicked <a> id.
+        // http://jsfiddle.net/abenrob/zkc5m/
+        function messageMarkerPopup(id){
+
+            that.map.eachLayer(function(layer){
+
+                if (layer.options !== undefined && layer.options.title == id) {
+                    that.map.setView([layer._latlng.lat, layer._latlng.lng], 7);
+                    layer.openPopup();
+                }
+            });
         }
-        // a must be equal to b
-        return 0;
-    });
+    }
 
-    // Clears message div to avoid duplicates after choosing category.
-    $("#messages").empty();
+    function sortMessages(messages) {
 
-    // Rendering messages in map-markers and list.
-    messages.forEach(function (message) {
+        messages.forEach(function(message){
+            // Changing format of date with moment.js.
+            message.createddate = moment(message.createddate).format("YYYY-MM-DD HH:mm");
+        });
 
-        var category;
-        var priority;
+        // Sorting by date and time.
+        messages.sort(function (a, b) {
+            if (a.createddate < b.createddate) {
+                return 1;
+            }
+            if (a.createddate > b.createddate) {
+                return -1;
+            }
+            // a must be equal to b
+            return 0;
+        });
+
+        return messages;
+    }
+
+    function filtrateMessages(messages){
+        var filtratedMessages = [];
+
+        messages.forEach(function(message) {
+
+            if (message.category === that.category) {
+                filtratedMessages.push(message);
+            }
+        });
+
+        return filtratedMessages;
+    }
+
+    function renderListItem(message, category, priority) {
+
+        var listItem = "<p><strong>"+message.subcategory+" - "+message.title+"</strong></p>" +
+            "<p class='chip'>"+category+"</p>" +
+            "<p class='date'>"+message.createddate+"</p>" +
+            "<p>"+message.description+"</p>";
+
+        // Render list
+        $("<li class='message-li'><a href='#' class='card-panel message hoverable' id='"+message.id+"'>"+listItem+"</a>" +
+            "</li>").appendTo("#messages");
+    }
+
+    function getPriorityAsString(priority) {
+
+        var priorityString;
+
+        switch (priority) {
+            case 1:
+                priorityString = "Myckel allvarlig händelse";
+                break;
+            case 2:
+                priorityString = "Stor händelse";
+                break;
+            case 3:
+                priorityString = "Störning";
+                break;
+            case 4:
+                priorityString = "Information";
+                break;
+            case 5:
+                priorityString = "Mindre störning";
+                break;
+        }
+
+        return priorityString;
+    }
+
+    function getCategoryAsString(message) {
+
+        var categoryString;
 
         // Assigning category based on number.
         switch (message.category){
             case 0:
-                category = "Vägtrafik";
+                categoryString = "Vägtrafik";
                 break;
             case 1:
-                category = "Kollektivtrafik";
+                categoryString = "Kollektivtrafik";
                 break;
             case 2:
-                category = "Planerad störning";
+                categoryString = "Planerad störning";
                 break;
             case 3:
-                category = "Övrigt";
+                categoryString = "Övrigt";
                 break;
         }
 
-        switch (message.priority) {
-            case 1:
-                priority = "Myckel allvarlig händelse";
-                break;
-            case 2:
-                priority = "Stor händelse";
-                break;
-            case 3:
-                priority = "Störning";
-                break;
-            case 4:
-                priority = "Information";
-                break;
-            case 5:
-                priority = "Mindre störning";
-                break;
-        }
+        return categoryString;
+    }
+
+    function createMarker(message, category, priority){
 
         var iconURL;
 
@@ -247,53 +305,39 @@ TrafficReporter.prototype.renderTrafficMessages = function(messages) {
                 iconURL = "icons/marker-icon-default.png"
         }
 
-
         var CustomIcon = L.Icon.Default.extend({
-           options: {
-               iconUrl: iconURL
-           }
+            options: {
+                iconUrl: iconURL
+            }
         });
 
         var customIcon = new CustomIcon();
 
-        // Adding marker to marker layer.
-        var marker = L.marker([message.latitude, message.longitude], { title: message.id, icon: customIcon }).addTo(that.markers).bindPopup(
-            "<strong>"+message.title+"</strong> (" + message.subcategory + ")" +
-            "<p>"+priority+"</p><p class='date'>" + message.createddate + "</p>" + message.description);
-
-
-        var listItem = "<p><strong>"+message.subcategory+" - "+message.title+"</strong></p>" +
-            "<p class='chip'>"+category+"</p>" +
-            "<p class='date'>"+message.createddate+"</p>" +
-            "<p>"+message.description+"</p>";
-
-        // Render list
-        $("<li class='message-li'><a href='#' class='card-panel message hoverable' id='"+message.id+"'>"+listItem+"</a>" +
-            "</li>").appendTo("#messages");
-
-    });
-
-    this.markers.addTo(this.map);
-
-    // Iterates through markers to find the one with same title as clicked <a> id.
-    // http://jsfiddle.net/abenrob/zkc5m/
-    function messageMarkerPopup(id){
-
-        that.map.eachLayer(function(layer){
-
-            if (layer.options !== undefined && layer.options.title == id) {
-                that.map.setView([layer._latlng.lat, layer._latlng.lng], 7);
-                layer.openPopup();
-            }
-        });
+        var marker = L.marker([message.latitude, message.longitude], { title: message.id, icon: customIcon }).addTo(that.markers).bindPopup("<strong>" + message.title +
+            "</strong><br>" + category + " (" + message.subcategory + ")<p class='date'>" + message.createddate + "</p>" + message.description);
     }
-
-    $(".message").click(function(){
-        messageMarkerPopup($(this).attr("id"));
-    });
-
-    $("#copyright").empty();
-    $("<small>"+copyright+"</small>").appendTo("#copyright");
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 window.onload = init();
